7c3633a8758075e4621ba74a6d6c175442380a3c
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-09-24 15:21:21 -0300

    [Feature Boaz -  Task: commit initial]

---
+++ src/main/java/com/blogpessoal/Turma34/Turma34Application.java
@@ -0,0 +1,13 @@
+package com.blogpessoal.Turma34;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Turma34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Turma34Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogpessoal/Turma34/Turma34ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogpessoal.Turma34;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Turma34ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
