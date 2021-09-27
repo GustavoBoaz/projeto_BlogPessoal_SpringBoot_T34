a27b7d3c3aa7da9fddf4637609870f6bf552f3b3
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-09-27 12:14:39 -0300

    [Feature Boaz - Task: ADD method salvar]

--- src/main/java/com/blogpessoal/Turma34/controladores/UsuarioControlador.java
+++ src/main/java/com/blogpessoal/Turma34/controladores/UsuarioControlador.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +41,11 @@ public class UsuarioControlador {
 		} else {
 			return ResponseEntity.status(204).build();
 		}
+	}
+	
+	@PostMapping("/salvar")
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario novoUsuario){
+		return ResponseEntity.status(201).body(repositorio.save(novoUsuario));
+		
 	}
 }
