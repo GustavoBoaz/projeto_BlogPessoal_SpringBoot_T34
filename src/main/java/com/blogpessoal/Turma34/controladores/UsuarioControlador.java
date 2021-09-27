fa1e52464cebc4897d7412389d5a07dce7e3dfd6
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-09-27 12:04:11 -0300

    [Feature Boaz - Task: ADD method pegarPorId]

--- src/main/java/com/blogpessoal/Turma34/controladores/UsuarioControlador.java
+++ src/main/java/com/blogpessoal/Turma34/controladores/UsuarioControlador.java
@@ -1,10 +1,12 @@
 package com.blogpessoal.Turma34.controladores;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,17 @@ public class UsuarioControlador {
 			return ResponseEntity.status(204).build();
 		} else {
 			return ResponseEntity.status(200).body(objetoLista);
+		}
+	}
+	
+	@GetMapping("/{id_usuario}")
+	public ResponseEntity<Usuario> pegarPorId(@PathVariable(value = "id_usuario") Long idUsuario ){
+		Optional<Usuario> objetoOptional = repositorio.findById(idUsuario);
+		
+		if (objetoOptional.isPresent()) {
+			return ResponseEntity.status(200).body(objetoOptional.get());
+		} else {
+			return ResponseEntity.status(204).build();
 		}
 	}
 }
