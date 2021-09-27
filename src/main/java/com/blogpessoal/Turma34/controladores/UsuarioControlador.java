5a1da15c7bf5a5491ff68666f23b0cc759ec727e
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-09-27 11:00:42 -0300

    [Feature Boaz - Task: ADD class UsuarioControlador]

---
+++ src/main/java/com/blogpessoal/Turma34/controladores/UsuarioControlador.java
@@ -0,0 +1,10 @@
+package com.blogpessoal.Turma34.controladores;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/usuario")
+public class UsuarioControlador {
+
+}
