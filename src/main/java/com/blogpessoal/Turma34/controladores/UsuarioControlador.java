ef0e41ece4f4759465dad773b1fff211be49fd81
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-09-27 12:35:14 -0300

    [Feature Boaz - Task: ADD method atualizar e deletar]

--- src/main/java/com/blogpessoal/Turma34/controladores/UsuarioControlador.java
+++ src/main/java/com/blogpessoal/Turma34/controladores/UsuarioControlador.java
@@ -7,9 +7,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +51,23 @@ public class UsuarioControlador {
 	public ResponseEntity<Usuario> salvar(@Valid @RequestBody Usuario novoUsuario){
 		return ResponseEntity.status(201).body(repositorio.save(novoUsuario));
 
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> atualizar(@Valid @RequestBody Usuario novoUsuario){
+		return ResponseEntity.status(201).body(repositorio.save(novoUsuario));
+
+	}
+	
+	@DeleteMapping("/deletar/{id_usuario}")
+	public ResponseEntity<Usuario> deletar(@PathVariable(value = "id_usuario") Long idUsuario ){
+		Optional<Usuario> objetoOptional = repositorio.findById(idUsuario);
+		
+		if (objetoOptional.isPresent()) {
+			repositorio.deleteById(idUsuario);
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(400).build();
+		}
 	}
 }
