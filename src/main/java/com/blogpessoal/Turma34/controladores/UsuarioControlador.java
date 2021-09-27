7bb3d941bd7da032568e6f1c0035d02a006ecd54
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-09-27 11:53:01 -0300

    [Feature Boaz - Task: ADD method pegarTodes]

--- src/main/java/com/blogpessoal/Turma34/controladores/UsuarioControlador.java
+++ src/main/java/com/blogpessoal/Turma34/controladores/UsuarioControlador.java
@@ -1,10 +1,30 @@
 package com.blogpessoal.Turma34.controladores;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.blogpessoal.Turma34.modelos.Usuario;
+import com.blogpessoal.Turma34.repositorios.UsuarioRepositorio;
 
 @RestController
 @RequestMapping("/api/v1/usuario")
 public class UsuarioControlador {
 
+	private @Autowired UsuarioRepositorio repositorio;
+	
+	@GetMapping("/todes")
+	public ResponseEntity<List<Usuario>> pegarTodes(){
+		List<Usuario> objetoLista = repositorio.findAll();
+		
+		if (objetoLista.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(objetoLista);
+		}
+	}
 }
