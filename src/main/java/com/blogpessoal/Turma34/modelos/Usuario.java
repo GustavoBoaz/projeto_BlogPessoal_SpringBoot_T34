5eecfedace1a9ebf21b20bf076178a08d554d494
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-09-27 09:58:34 -0300

    [Feature Boaz - Task: Implemented class Usuario]

--- src/main/java/com/blogpessoal/Turma34/modelos/Usuario.java
+++ src/main/java/com/blogpessoal/Turma34/modelos/Usuario.java
@@ -1,10 +1,55 @@
 package com.blogpessoal.Turma34.modelos;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Classe espelho da tabela usuario no banco db_blogpessoal.
+ * 
+ * @author Turma34
+ * @since 1.0
+ *
+ */
+@Entity
 public class Usuario {
-	
-	private Long idUsuario;
+
+	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long idUsuario;
 	private String nome;
 	private String email;
 	private String senha;
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 }
