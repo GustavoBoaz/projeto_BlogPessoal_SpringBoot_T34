2652a47efa906fe87be021b7462a7fae5dcf21b8
Lucas Reis <88865748+therealkaslow@users.noreply.github.com>
2021-09-30 09:19:11 -0300

    [Feature Lucas - Task: REMOVE methods cadastrarI and II]

--- src/main/java/com/blogpessoal/Turma34/servicos/UsuarioServicos.java
+++ src/main/java/com/blogpessoal/Turma34/servicos/UsuarioServicos.java
@@ -32,40 +32,4 @@ public class UsuarioServicos {
 
 	}
 
-	/**
-	 * Metodo utilizado para cadastrar usuario validando duplicidade de email no
-	 * banco
-	 * 
-	 * @param usuarioParaCadastrar do tipo Usuario
-	 * @return Optional com Usuario cadastrado caso email não seja existente
-	 * @author Turma34
-	 * @since 1.5
-	 * 
-	 */
-	public Optional<Object> cadastrarUsuarioI(Usuario usuarioParaCadastrar) {
-		return repositorio.findByEmail(usuarioParaCadastrar.getEmail()).map(usuarioExistente -> Optional.empty())
-				.orElse(Optional.ofNullable(repositorio.save(usuarioParaCadastrar)));
-
-	}
-
-	/**
-	 * Metodo utilizado para cadastrar usuario validando duplicidade de email no
-	 * banco
-	 * 
-	 * @param usuarioParaCadastrar do tipo Usuario
-	 * @return Optional com Usuario cadastrado caso email não seja existente
-	 * @author Turma34
-	 * @since 1.0
-	 * 
-	 */
-	public Optional<Object> cadastrarUsuarioII(Usuario usuarioParaCadastrar) {
-		Optional<Usuario> objetoOptional = repositorio.findByEmail(usuarioParaCadastrar.getEmail());
-
-		if (objetoOptional.isPresent()) {
-			return Optional.empty();
-		} else {
-			return Optional.ofNullable(repositorio.save(usuarioParaCadastrar));
-		}
-
-	}
 }
