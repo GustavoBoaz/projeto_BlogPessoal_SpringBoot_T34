9d126aa5da2b4dd0b36a01905c8fabef8ad2d26e
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-10-06 15:17:41 -0300

    [Feature Boaz - Task: MODIFY method configure http]

--- src/main/java/com/blogpessoal/Turma34/seguranca/BasicSecurityConfig.java
+++ src/main/java/com/blogpessoal/Turma34/seguranca/BasicSecurityConfig.java
@@ -27,6 +27,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 		.authorizeRequests()
 			.antMatchers(HttpMethod.POST, "/api/v1/usuario/salvar").permitAll()
+			.antMatchers(HttpMethod.PUT, "/api/v1/usuario/credenciais").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
