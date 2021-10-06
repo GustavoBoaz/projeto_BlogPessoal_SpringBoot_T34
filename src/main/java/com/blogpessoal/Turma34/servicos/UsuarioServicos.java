5280ba3ac2ef9fc2e7d41fc40cee8b585132b603
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-10-06 15:05:19 -0300

    [Feature Boaz - Task: ADD method pegarCredenciais]

--- src/main/java/com/blogpessoal/Turma34/servicos/UsuarioServicos.java
+++ src/main/java/com/blogpessoal/Turma34/servicos/UsuarioServicos.java
@@ -1,12 +1,15 @@
 package com.blogpessoal.Turma34.servicos;
 
+import java.nio.charset.Charset;
 import java.util.Optional;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.blogpessoal.Turma34.modelos.Usuario;
+import com.blogpessoal.Turma34.modelos.dtos.UsuarioLoginDTO;
 import com.blogpessoal.Turma34.repositorios.UsuarioRepositorio;
 
 @Service
@@ -28,14 +31,14 @@ public class UsuarioServicos {
 		return repositorio.findByEmail(usuarioParaCadastrar.getEmail()).map(usuarioExistente -> {
 			return Optional.empty();
 		}).orElseGet(() -> {
-			BCryptPasswordEncoder encoder =  new BCryptPasswordEncoder();
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 			String senhaCriptografada = encoder.encode(usuarioParaCadastrar.getSenha());
 			usuarioParaCadastrar.setSenha(senhaCriptografada);
 			return Optional.ofNullable(repositorio.save(usuarioParaCadastrar));
 		});
 
 	}
-	
+
 	/**
 	 * Metodo utilizado para atualizar usuario no banco
 	 * 
@@ -45,7 +48,7 @@ public class UsuarioServicos {
 	 * @since 1.0
 	 * 
 	 */
-	public Optional<Usuario> atualizarUsuario(Usuario usuarioParaAtualizar){
+	public Optional<Usuario> atualizarUsuario(Usuario usuarioParaAtualizar) {
 		return repositorio.findById(usuarioParaAtualizar.getIdUsuario()).map(resp -> {
 			resp.setNome(usuarioParaAtualizar.getNome());
 			resp.setSenha(usuarioParaAtualizar.getSenha());
@@ -53,5 +56,41 @@ public class UsuarioServicos {
 		}).orElseGet(() -> {
 			return Optional.empty();
 		});
+	}
+
+	/**
+	 * Metodo utilizado para pegar credenciais do usuario com Tokem (Formato Basic),
+	 * este método sera utilizado para retornar ao front o token utilizado para ter
+	 * acesso aos dados do usuario e mantelo logado no sistema
+	 * 
+	 * @param usuarioParaAutenticar do tipo UsuarioLoginDTO necessario email e senha
+	 *                              para validar
+	 * @return UsuarioLoginDTO preenchido com informações mais o Token
+	 * @since 1.0
+	 * @author Turma34
+	 */
+	public Optional<?> pegarCredenciais(UsuarioLoginDTO usuarioParaAutenticar) {
+		return repositorio.findByEmail(usuarioParaAutenticar.getEmail()).map(resp -> {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+			if (encoder.matches(usuarioParaAutenticar.getSenha(), resp.getSenha())) {
+
+				String estruturaBasic = usuarioParaAutenticar.getEmail() + ":" + usuarioParaAutenticar.getSenha(); // gustavoboaz@gmail.com:134652
+				byte[] estruturaBase64 = Base64.encodeBase64(estruturaBasic.getBytes(Charset.forName("US-ASCII"))); // hHJyigo-o+i7%0ÍUG465sas=-
+				String tokenBasic = "Basic " + new String(estruturaBase64); // Basic hHJyigo-o+i7%0ÍUG465sas=-
+
+				usuarioParaAutenticar.setToken(tokenBasic);
+				usuarioParaAutenticar.setIdUsuario(resp.getIdUsuario());
+				usuarioParaAutenticar.setNome(resp.getNome());
+				usuarioParaAutenticar.setSenha(resp.getSenha());
+
+				return Optional.ofNullable(usuarioParaAutenticar); // Usuario Credenciado
+			} else {
+				return Optional.empty(); // Senha incorreta
+			}
+		}).orElseGet(() -> {
+			return Optional.empty(); // Email não existe
+		});
+
 	}
 }
