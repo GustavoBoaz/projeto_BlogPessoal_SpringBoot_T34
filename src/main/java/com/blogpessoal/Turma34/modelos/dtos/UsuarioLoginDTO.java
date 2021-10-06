d5c97425258fc88bdf572e5d434730387cd7e165
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-10-06 14:48:51 -0300

    [Feature Boaz - Task: ADD class DTO UsuarioLoginDTO]

---
+++ src/main/java/com/blogpessoal/Turma34/modelos/dtos/UsuarioLoginDTO.java
@@ -0,0 +1,51 @@
+package com.blogpessoal.Turma34.modelos.dtos;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * Classe Espelho para logar no sistema
+ * 
+ * @author Turma34
+ *
+ */
+public class UsuarioLoginDTO {
+
+	private @NotBlank String email; // Necessario Para Login
+	private @NotBlank String senha; // Necessario Para Login
+
+	private Long idUsuario;
+	private String nome;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
