372cab5ee71bbfeed3a8bf7df369057de1d86863
Gustavo Boaz <gustavo.boaz@hotmail.com>
2021-10-14 12:29:48 -0300

    [Feature Boaz - Task: ADD redirect for swagger]

--- src/main/java/com/blogpessoal/Turma34/Turma34Application.java
+++ src/main/java/com/blogpessoal/Turma34/Turma34Application.java
@@ -2,9 +2,20 @@ package com.blogpessoal.Turma34;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class Turma34Application {
+
+	@GetMapping
+	public ModelAndView swaggerUi() {
+		return new ModelAndView("redirect:/swagger-ui/");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Turma34Application.class, args);
